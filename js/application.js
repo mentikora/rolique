381420e326d9b9bfe62bdf69b4a501d30abef630
horo <notender_boy@ukr.net>
2016-09-08 18:50:02 +0300

    anim

--- js/application.js
+++ js/application.js
@@ -11,4 +11,44 @@
 	    $preloader.delay(5000).fadeOut('slow');
 	});
 
+  var colors = ['red', 'black', 'white', 'green', 'yellow', 'orange', 'brown', 'inherit']; 
+  function animation(i) {
+    if (i > colors.length) {
+      return
+    } 
+    
+    var options = {
+        duration: 500,
+        easing: 'linear',
+        complete: function(){
+          // if (i >= colors.length){i = 0;}
+          $(this).css('background', colors[i])
+         i++;
+       }
+     };
+     $('.test_wrapper')
+        .find('.test')
+        .animate({
+              left: 280
+           },
+           options
+        )
+
+        .animate({
+              left: 0,
+           },
+           $.extend(true, {}, options, {
+              complete: function() {
+                // if (i >= colors.length){i = 0;}
+                $(this).css('background', colors[i])
+               i++;
+                 animation(i);
+              }
+           })
+        );
+  };
+
+  animation(0)
+
+
 })();
