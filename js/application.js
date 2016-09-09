63a3f8cc7c5115314ac6a8c41c7304ec026811e4
Mentikora <notender_boy@ukr.net>
2016-09-10 02:04:48 +0300

    update

--- js/application.js
+++ js/application.js
@@ -1,15 +1,18 @@
 "use strict";
 
+
+
+
 (function(){
 
-	// init WoW
-	new WOW().init();
+  // init WoW
+  new WOW().init();
 
-	// preload
-	$(window).on('load', function() {
-	    var $preloader = $('.preloader');
-	    $preloader.delay(7000).fadeOut('slow');
-	});
+  // preload
+  $(window).on('load', function() {
+      var $preloader = $('.preloader');
+      $preloader.delay(6000).fadeOut('slow');
+  });
 
   // preload animation
   var colors = ['red', 'black', 'white', 'green', 'yellow', 'orange', 'brown', 'blue', 'grey', 'inherit']; 
@@ -17,39 +20,37 @@
     if (i > colors.length) {
       return
     } 
-    
     var options = {
-        duration: 500,
-        easing: 'linear',
-        complete: function(){
-          // if (i >= colors.length){i = 0;}
-          $(this).css('background', colors[i])
-         i++;
-       }
-     };
-     $('.test_wrapper')
-        .find('.test')
-        .animate({
-              left: 280
-           },
-           options
-        )
-
-        .animate({
-              left: 0,
-           },
-           $.extend(true, {}, options, {
-              complete: function() {
-                // if (i >= colors.length){i = 0;}
-                $(this).css('background', colors[i])
-               i++;
-                 animation(i);
-              }
-           })
-        );
+      duration: 500,
+      easing: 'linear',
+      complete: function(){
+        $(this).css('background', colors[i])
+       i++;
+     }
+   };
+   $('.animation_wrapper')
+    .find('.animation')
+    .animate({ left: 280 }, options)
+    .animate({ left: 0,},
+    $.extend(true, {}, options, {
+      complete: function() {
+        $(this).css('background', colors[i]);
+        i++;
+        animation(i);
+      }
+     })
+    );
   };
 
   animation(0)
+
+  // scroll to section
+  $('#menu span').on('click', function(){
+    var data = $(this).data('scroll');
+    $('html, body').animate({
+      scrollTop: $(data).offset().top
+    }, 800);
+  });
 
 
 })();
